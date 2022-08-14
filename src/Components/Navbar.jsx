0cba2813c30fc3bc5e352c7b3bdac1eb54d3b92f
Raushan Kumar <raushansft@gmail.com>
2022-08-14 20:02:23 +0530

    component

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Navbar</h1>
+        
+      </div>
+    )
+  }
+}
---
+++ src/Components/NewCard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewCard extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
